3de595f2ce496712e124096077594bd6ea9325bc
Igor <qwertyhock@gmail.com>
2023-09-14 12:57:01 +0300

    14.09.23

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
-import React from 'react';
+import React from 'react'
 import './global.scss'
-import { Layout } from './shared/Layout/Layout.tsx';
-import { Header } from './shared/Header/Header.tsx';
-import { Content } from './shared/Content/Content.tsx';
-import { CardsList } from './shared/CardsList/CardsList.tsx';
+import { Layout } from './shared/Layout/Layout.tsx'
+import { Header } from './shared/Header/Header.tsx'
+import { Content } from './shared/Content/Content.tsx'
+import { CardsList } from './shared/CardsList/CardsList.tsx'
 
 export const App = () => {
-    return (
-        <Layout>
-            <Header />
-            <Content>
-                <CardsList />
-            </Content>
-        </Layout>
-    );
-};
+  return (
+    <Layout>
+      <Header />
+      <Content>
+        <CardsList />
+      </Content>
+    </Layout>
+  )
+}
 
-export default App;
+export default App
--- src/shared/CardsList/Card/PostMenuAction/PostMenuAction.tsx
+++ src/shared/CardsList/Card/PostMenuAction/PostMenuAction.tsx
@@ -1,23 +1,36 @@
 import React from 'react'
 import styles from './postmenuaction.scss'
 import { PostControls } from './PostControls/PostControls.tsx'
+import { Dropdown } from '../Dropdown/Dropdown.tsx'
 
 export function PostMenuAction() {
   return (
     <div className={styles.menu}>
-      <button className={styles.menuButton}>
-        <svg
-          width="5"
-          height="20"
-          viewBox="0 0 5 20"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <circle cx="2.5" cy="2.5" r="2.5" fill="#D9D9D9" />
-          <circle cx="2.5" cy="10" r="2.5" fill="#D9D9D9" />
-          <circle cx="2.5" cy="17.5" r="2.5" fill="#D9D9D9" />
-        </svg>
-      </button>
+      <Dropdown
+        onClose={() => console.log('closed')}
+        onOpen={() => console.log('opened')}
+        isOpen={false}
+        button={
+          <button className={styles.menuButton}>
+            <svg
+              width="5"
+              height="20"
+              viewBox="0 0 5 20"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <circle cx="2.5" cy="2.5" r="2.5" fill="#D9D9D9" />
+              <circle cx="2.5" cy="10" r="2.5" fill="#D9D9D9" />
+              <circle cx="2.5" cy="17.5" r="2.5" fill="#D9D9D9" />
+            </svg>
+          </button>
+        }
+      >
+        <ul>
+          <li onClick={console.log}>Click</li>
+          <li>Dont Click</li>
+        </ul>
+      </Dropdown>
       <PostControls />
     </div>
   )
